ea9927255f0a9e01e95e9cb063651e8d62ebd981
Thorsten Ludewig <th@l9g.de>
2020-06-18 08:02:21 +0200

    remove the App header file from gitignore

---
+++ lib/App/App.hpp
@@ -0,0 +1,23 @@
+#ifndef __APP_H__
+#define __APP_H__
+
+#include <Arduino.h>
+#include <AppConfig.h>
+
+#define LOG0( format ) Serial.printf( "(%ld) " format, millis())
+#define LOG1( format, x) Serial.printf( "(%ld) " format, millis(), x )
+#define TLOG0( format ) TelnetStream.printf( "(%ld) " format, millis())
+#define TLOG1( format, x) TelnetStream.printf( "(%ld) " format, millis(), x )
+
+#define APP_NAME "ESP8266 Arduino Info"
+#define APP_VERSION "0.8.4"
+#define APP_AUTHOR "Dr. Thorsten Ludewig <t.ludewig@gmail.com>"
+#define APP_CONFIG_FILE "/config.bin"
+
+extern void appSetup();
+extern void appLoop();
+extern void appShowHeader(Stream& out);
+extern const char *appUptime();
+extern const char *appDateTime();
+
+#endif
